29a3b73a44c29943dcc0ce78326c8364c4503a96
lucaslagant <lucaslagant@gmail.com>
2023-01-26 10:27:52 +0100

    gestion session co / deco

--- controllers/connexion-ctrl.php
+++ controllers/connexion-ctrl.php
@@ -4,42 +4,6 @@ $bdd = new PDO('mysql:host=localhost; dbname=amiens_sc', 'admin' , 'Parker.22807
 // Appelle d'un fichier init qui contient juste un session_start 
 require_once(dirname(__FILE__).'/../utils/init.php');
 
-// $errorsArray = [];
-
-// if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST)) {
-//     $email = trim(filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL));
-//     $isOk = filter_var($email, FILTER_SANITIZE_EMAIL);
-
-//     // FILTER_SANITIZE_EMAIL supprime tout les caractéres interdit dans l'email
-
-//     if (!empty($email)) {
-//         if (!$isOk) {
-//             $errorsArray['email_error'] = 'Le mail n\est pas valide';
-//         }
-//     }else {
-//         $errorsArray['email_error'] = 'Le champ est obligatoire';
-//     }
-//     $mdp = $_POST['mdp'];
-//     // sha1() fait matcher le cryptage saisie avec celui de la bdd
-//     if (empty($errorsArray)) {
-//         $recupUser = $bdd->prepare('SELECT * FROM user WHERE email = ?');
-//         $recupUser->execute(array($email));
-//         // Si déja un user qui existe
-//         // On vérifie le mot de passe 
-//         var_dump(password_verify($mdp));die;
-        
-        
-//         if ($recupUser-> rowCount() > 0) {
-//             $_SESSION['email'] = $email;
-//             $_SESSION['mdp'] = $mdp;
-//             $_SESSION['id'] = $recupUser->fetch()['id'];
-//             header('Location: index.php');
-//         }else {
-//             echo "Erreur !!!";
-//         }
-//     }
- 
-// }
 if (!empty($_POST)) {
     if (isset($_POST["email"], $_POST["mdp"]) && !empty($_POST["email"] && !empty($_POST["mdp"]))) {
     //    On vérifie que l'email en est un 
@@ -70,8 +34,8 @@ if (!empty($_POST)) {
             "prenom" => $user["prenom"],
             "email" => $user["email"]
         ];
-        // On redirige vers la page de profil
-        header("Location: /../homeconnect.php");
+        // On redirige vers la page de index
+        header("Location: /../index");
 
        
     }
--- controllers/inscription-ctrl.php
+++ controllers/inscription-ctrl.php
@@ -6,52 +6,6 @@ require_once(dirname(__FILE__).'/../utils/init.php');
 // Appelle d'un fichier contenant toute les regex 
 require_once(dirname(__FILE__). '/../utils/regex.php');
 
-    // Création d'un tableau d'erreur
-    $errors = [];
-
-    // vérification du form
-
-    // if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST))
-    // {
-    //     // Vérification et sécurité trim = supprime les caractères et espace en début et fin de chaine, filter_input = recup une variable externe et filtre 
-    //     $nom = trim(filter_input(INPUT_POST , 'nom'));
-    //     if ($nom) {
-    //         // Gestion des erreurs
-    //         if (!preg_match(REGEX_NO_NUMBER , $nom)) {
-    //             $errors['nom'] = 'Format du nom invalide';
-    //         }
-    //         // /!\ Ne pas oublier de rajouter un pattern  avec la regex dans la views pour une double vérification 
-    //     }
-
-    //     $prenom = trim(filter_input(INPUT_POST , 'prenom'));
-    //     if ($prenom) {
-    //         if (!preg_match(REGEX_NO_NUMBER , $prenom)) {
-    //             $errors['prenom'] = 'Format du prénom invalide';
-    //         }
-    //     }
-        
-       
-    //     // Si le tableau d'erreur est vide 
-    //     if (empty($error)) {            
-
-    //         // recuperation de l'utilisateur en bdd 
-    //         $recupUser = $bdd->prepare('SELECT * FROM user WHERE nom = ? AND prenom = ? AND mdp = ?;');
-    //         $recupUser->execute(array($nom,$prenom,$mdp));
-
-    //         // On recupère l'id du nouvelle utilisateur 
-    //         $id = $bdd->lastInsertId();
-    //         // Si un user existe déjà 
-    //         if ($recupUser->rowCount() > 0) {
-    //             $_SESSION['nom'] = $nom;
-    //             $_SESSION['prenom'] = $prenom;
-    //             $_SESSION['id'] = $recupUser->fetch()['id'];
-    //         }
-    //         header('Location: /../index.php');
-    //     }
-
-       
-    // }
-
     if (!empty($_POST)) {
         if (isset($_POST["nom"], $_POST["prenom"], $_POST["email"], $_POST["mdp"]) && !empty($_POST["nom"]) && !empty($_POST["prenom"]) && !empty($_POST["email"]) &&  !empty($_POST["mdp"]) ) {
             // Le form est complet 
