bf65d7d3eb117b3ed0fc2ac79c4d2193d796ba66
qsaval <quentin.saval@hotmail.fr>
2023-01-26 12:07:06 +0100

    modif form

--- form_modif_user.php
+++ form_modif_user.php
@@ -2,7 +2,7 @@
     include "db.php";
     $db = connexionBase();
 
-    $requete = $db->query("SELECT * FROM user WHERE nom = ''");
+    $requete = $db->query("SELECT * FROM user");
     $tab = $requete->fetchAll(PDO::FETCH_OBJ);
     $requete->closeCursor();
 ?>
@@ -40,51 +40,51 @@
             <div id="sidemenu" class="invisible"><?php include('Page_Navbar.php'); ?></div>
         </div>
     </nav>
-    
-    <?php foreach ($tab as $user):?>
+
     <section class="bg-rose-50">
 
         <div class="">
             <div class="flex justify-center text-rose-800 pt-10 pb-10">
                 <div class="inline-block shadow-lg bg-rose-100 p-8 px-32 m-4">
                 <form action="">
-
+                    
                     <span class="text-3xl">Modifier les information</span>
                     <br><br><br>
-
+                    
+                    <?php foreach($tab as $user):?>
                     <div class="flex flex-col">
-                    <label for="nom">Nom : </label>
-                    <input type="text" id="nom"  class="rounded border-rose-300 border-2">
-                    <br><br>
-
-                    <label for="nom">Prénom : </label>
-                    <input type="text" id="nom" class="rounded border-rose-300 border-2">
-                    <br><br>
-
-                    <label for="nom">Email : </label>
-                    <input type="text" id="nom" class="rounded border-rose-300 border-2">
-                    <br><br>
-
-                    <label for="nom">Mot de passe : </label>
-                    <input type="text" id="nom" class="rounded border-rose-300 border-2">
-                    <br><br>
-
-                    <label for="nom">Réécrivez le mot de passe : </label>
-                    <input type="text" id="nom" class="rounded border-rose-300 border-2">
-                    <br><br>
-                    </div>
-
-                    <div class="flex justify-center">
-                    <input type="submit" value="Créer" class="px-12 block text-rose-50 inline-block mt-0 hover:text-white bg-rose-800 hover:bg-rose-600 px-5 py-2 rounded-full">
-                    <a href="Connexion.php" class="ml-6 block text-rose-800 inline-block mt-0 hover:text-rose-800 bg-rose-300 hover:bg-white px-5 py-2 rounded-full">Se connecter</a>
-                    </div>
+                        <label for="nom">Nom : </label>
+                            <input type="text" id="nom" value="<?= $user->nom ?>" class="rounded border-rose-300 border-2">
+                            <br><br>
+                            
+                            <label for="nom">Prénom : </label>
+                            <input type="text" id="nom" value="<?= $user->prenom ?>" class="rounded border-rose-300 border-2">
+                            <br><br>
+                            
+                            <label for="nom">Email : </label>
+                            <input type="text" id="nom" value="<?= $user->email ?>" class="rounded border-rose-300 border-2">
+                            <br><br>
+                            
+                            <label for="nom">Mot de passe : </label>
+                            <input type="text" id="nom" value="<?= $user->mdp ?>" class="rounded border-rose-300 border-2">
+                            <br><br>
+                            
+                            <label for="nom">Réécrivez le mot de passe : </label>
+                            <input type="text" id="nom" value="<?= $user->mdp ?>" class="rounded border-rose-300 border-2">
+                            <br><br>
+                        </div>
+                        
+                        <div class="flex justify-center">
+                            <input type="submit" value="Créer" class="px-12 block text-rose-50 inline-block mt-0 hover:text-white bg-rose-800 hover:bg-rose-600 px-5 py-2 rounded-full">
+                            <a href="Connexion.php" class="ml-6 block text-rose-800 inline-block mt-0 hover:text-rose-800 bg-rose-300 hover:bg-white px-5 py-2 rounded-full">Se connecter</a>
+                        </div>
+                        <?php endforeach; ?>
                 </form>
+                </div>
             </div>
         </div>
-    </div>
-    
-</section>
-<?php endforeach; ?>
+
+    </section>
 
 
     <!-- Footer -->
