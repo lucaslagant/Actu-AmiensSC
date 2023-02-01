2ed12b3e1e3587b71a75a707b650bda144edec2c
qsaval <quentin.saval@hotmail.fr>
2023-02-01 16:44:30 +0100

    modif

--- pages/transfert.php
+++ pages/transfert.php
@@ -38,8 +38,8 @@
         </div>
     </nav>
 
-        <div class="btn">
-            <svg class="h-8 w-8 text-rose-500 icon"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  
+        <div class="btn text-rose-500 hover:text-rose-400">
+            <svg class="h-8 w-8 icon"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  
                 <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="12" r="9" />  <line x1="12" y1="8" x2="8" y2="12" />  <line x1="12" y1="8" x2="12" y2="16" />  <line x1="16" y1="12" x2="12" y2="8" />
             </svg>
         </div>
