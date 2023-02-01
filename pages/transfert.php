68b82fa4b5024ebfc6bc14d3365cd9f95f400e3d
qsaval <quentin.saval@hotmail.fr>
2023-02-01 13:04:24 +0100

    modif couleur

--- pages/transfert.php
+++ pages/transfert.php
@@ -39,7 +39,7 @@
     </nav>
 
         <a href="#" class="btn">
-            <svg class="h-8 w-8 text-rose-800 icon"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  
+            <svg class="h-8 w-8 text-rose-500 icon"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  
                 <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="12" r="9" />  <line x1="12" y1="8" x2="8" y2="12" />  <line x1="12" y1="8" x2="12" y2="16" />  <line x1="16" y1="12" x2="12" y2="8" />
             </svg>
         </a>
