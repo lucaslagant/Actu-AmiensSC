af64300f66988af936540ba7f4811275a3db048d
qsaval <quentin.saval@hotmail.fr>
2023-02-01 11:04:57 +0100

    nouvelle organisation

--- pages/transfert.php
+++ pages/transfert.php
@@ -1,5 +1,5 @@
 <?php
-    include "db.php";
+    include ('db.php');
     $db = connexionBase();
 
     $requete = $db->query("SELECT * FROM transferts_2022_2023 JOIN joueurs ON joueurs.id = transferts_2022_2023.joueurs_id ORDER BY transferts_2022_2023.id ASC");
@@ -21,7 +21,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link href='transfert.css' rel='stylesheet'>
+    <link href='assets/css/transfert.css' rel='stylesheet'>
     <title>Transfert</title>
 </head>
 <body style="background-color:#ffe4e6">
@@ -34,7 +34,7 @@
             <div>
                 <button class="block text-xl text-rose-100 inline-block mt-0 hover:text-white bg-rose-800 hover:bg-rose-700 px-5 py-2 rounded-full" id="burger"><i class='bx bx-menu'></i></button>
             </div>
-            <div id="sidemenu" class="invisible"><?php include('Page_Navbar.php'); ?></div>
+            <div id="sidemenu" class="invisible"><?php include('templates/Page_Navbar.php'); ?></div>
         </div>
     </nav>
 
@@ -92,8 +92,8 @@
             <?php endforeach; ?>   
         <?php endforeach; ?>   
 
-        <?php include('Footer.php'); ?>
+        <?php include('templates/Footer.php'); ?>
     <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
-    <script src="transfert.js"></script>
+    <script src="assets/js/transfert.js"></script>
 </body>
 </html>
