f0b50e7511f6e2bf6dbb4dff24b27a9001870f2c
qsaval <quentin.saval@hotmail.fr>
2023-02-01 09:24:53 +0100

    rajout des fichers css et js pour les transferts

--- transfert.php
+++ transfert.php
@@ -21,6 +21,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href='transfert.css' rel='stylesheet'>
     <title>Transfert</title>
 </head>
 <body style="background-color:#ffe4e6">
@@ -36,6 +37,12 @@
             <div id="sidemenu" class="invisible"><?php include('Page_Navbar.php'); ?></div>
         </div>
     </nav>
+
+        <a href="#" class="btn">
+            <svg class="h-8 w-8 text-rose-800 icon"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  
+                <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="12" r="9" />  <line x1="12" y1="8" x2="8" y2="12" />  <line x1="12" y1="8" x2="12" y2="16" />  <line x1="16" y1="12" x2="12" y2="8" />
+            </svg>
+        </a>
         <br>
         <?php foreach($tab as $transf): ?>
             <?php foreach($tab1 as $image): ?>
@@ -87,5 +94,6 @@
 
         <?php include('Footer.php'); ?>
     <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+    <script src="transfert.js"></script>
 </body>
 </html>
